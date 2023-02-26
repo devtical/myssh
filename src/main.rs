cf09e4dd704221120c455b3eb528c3a6c6ca80e4
Wahyu Kristianto <w.kristories@gmail.com>
2023-02-27 05:34:49 +0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use cursive::align::HAlign;
-use cursive::traits::*;
-use cursive::views::{Dialog, SelectView, TextView};
-use cursive::Cursive;
-use std::fs;
-use std::path::Path;
+use cursive::{align::HAlign, traits::*, views::*, Cursive};
+use std::{env, fs, path::Path};
 
 fn main() {
-    let home = std::env::var("HOME").unwrap();
+    let home: String = env::var("HOME").unwrap();
     let path = Path::new(&home).join(".ssh");
-    let mut arr = Vec::new();
+    let mut arr: Vec<String> = Vec::new();
 
     for entry in fs::read_dir(path).expect("Unable to list") {
         let entry = entry.expect("unable to get entry");
@@ -20,14 +16,17 @@ fn main() {
         }
     }
 
-    let mut select = SelectView::new().h_align(HAlign::Center).autojump();
+    let mut select = SelectView::<String>::new()
+        .h_align(HAlign::Center)
+        .autojump();
 
-    select.add_all_str(arr);
+    select.add_all(arr);
     select.set_on_submit(show_next_window);
 
-    let mut siv = cursive::default();
-
-    siv.add_layer(Dialog::around(select.scrollable().fixed_size((40, 10))).title("My Keys"));
+    let mut siv = Cursive::default();
+    siv.add_layer(
+        Dialog::around(select.scrollable().fixed_size((40, 10))).title("My Keys"),
+    );
     siv.run();
 }
 
@@ -35,7 +34,9 @@ fn show_next_window(siv: &mut Cursive, str: &str) {
     siv.pop_layer();
 
     let contents = fs::read_to_string(str).expect("Should have been able to read the file");
-    let text = contents;
+    let text: String = contents;
 
-    siv.add_layer(Dialog::around(TextView::new(text)).button("Quit", |s| s.quit()));
+    siv.add_layer(
+        Dialog::around(TextView::new(text)).button("Quit", |s| s.quit()),
+    );
 }
