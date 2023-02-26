3d7f3a605e8446dfbb8cda5b7010a95aa5a97c04
Wahyu Kristianto <w.kristories@gmail.com>
2023-02-27 05:47:08 +0700

    Rollback

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let home: String = std::env::var("HOME").unwrap();
-    let path: &Path = Path::new(&home).join(".ssh");
-    let mut arr: Vec<(String, String)> = Vec::new();
+    let home = std::env::var("HOME").unwrap();
+    let path = Path::new(&home).join(".ssh");
+    let mut arr = Vec::new();
 
     for entry in fs::read_dir(path).expect("Unable to list") {
         let entry = entry.expect("unable to get entry");
@@ -16,25 +16,25 @@ fn main() {
         let str = path.display().to_string();
 
         if !str.contains("known_hosts") {
-            arr.push((str.clone(), str));
+            arr.push(str);
         }
     }
 
-    let mut select = SelectView::<(String, String)>::new().h_align(HAlign::Center).autojump();
+    let mut select = SelectView::new().h_align(HAlign::Center).autojump();
 
-    select.add_all(arr);
+    select.add_all_str(arr);
     select.set_on_submit(show_next_window);
 
-    let mut siv = Cursive::default();
+    let mut siv = cursive::default();
 
     siv.add_layer(Dialog::around(select.scrollable().fixed_size((40, 10))).title("My Keys"));
     siv.run();
 }
 
-fn show_next_window(siv: &mut Cursive, str: &(String, String)) {
+fn show_next_window(siv: &mut Cursive, str: &str) {
     siv.pop_layer();
 
-    let contents = fs::read_to_string(&str.1).expect("Should have been able to read the file");
+    let contents = fs::read_to_string(str).expect("Should have been able to read the file");
     let text = contents;
 
     siv.add_layer(Dialog::around(TextView::new(text)).button("Quit", |s| s.quit()));
