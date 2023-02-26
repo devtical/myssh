540dc7fff1e3e55ee48f5b1162c9b96e44077fcc
Wahyu Kristianto <w.kristories@gmail.com>
2023-02-27 05:39:57 +0700

    Fix data tipe & paramter, runner

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use cursive::{align::HAlign, traits::*, views::*, Cursive};
-use std::{env, fs, path::Path};
+use cursive::align::HAlign;
+use cursive::traits::*;
+use cursive::views::{Dialog, SelectView, TextView};
+use cursive::Cursive;
+use std::fs;
+use std::path::Path;
 
 fn main() {
-    let home: String = env::var("HOME").unwrap();
-    let path = Path::new(&home).join(".ssh");
-    let mut arr: Vec<String> = Vec::new();
+    let home: String = std::env::var("HOME").unwrap();
+    let path: &Path = Path::new(&home).join(".ssh");
+    let mut arr: Vec<(String, String)> = Vec::new();
 
     for entry in fs::read_dir(path).expect("Unable to list") {
         let entry = entry.expect("unable to get entry");
@@ -12,31 +16,26 @@ fn main() {
         let str = path.display().to_string();
 
         if !str.contains("known_hosts") {
-            arr.push(str);
+            arr.push((str.clone(), str));
         }
     }
 
-    let mut select = SelectView::<String>::new()
-        .h_align(HAlign::Center)
-        .autojump();
+    let mut select = SelectView::<(String, String)>::new().h_align(HAlign::Center).autojump();
 
     select.add_all(arr);
     select.set_on_submit(show_next_window);
 
     let mut siv = Cursive::default();
-    siv.add_layer(
-        Dialog::around(select.scrollable().fixed_size((40, 10))).title("My Keys"),
-    );
+
+    siv.add_layer(Dialog::around(select.scrollable().fixed_size((40, 10))).title("My Keys"));
     siv.run();
 }
 
-fn show_next_window(siv: &mut Cursive, str: &str) {
+fn show_next_window(siv: &mut Cursive, str: &(String, String)) {
     siv.pop_layer();
 
-    let contents = fs::read_to_string(str).expect("Should have been able to read the file");
-    let text: String = contents;
+    let contents = fs::read_to_string(&str.1).expect("Should have been able to read the file");
+    let text = contents;
 
-    siv.add_layer(
-        Dialog::around(TextView::new(text)).button("Quit", |s| s.quit()),
-    );
+    siv.add_layer(Dialog::around(TextView::new(text)).button("Quit", |s| s.quit()));
 }
